714c5fa712608537b0171e6a6818590ac6c9ae98
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2022-03-07 19:54:28 -0300

    Criação do vetor e construtor

---
+++ Main.java
@@ -0,0 +1,13 @@
+package loianeGroner_EstruturaDeDadosComJAVA;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+
+		int i = 0;
+		i++;
+
+	}
+
+}
--- Vetor.java
+++ Vetor.java
@@ -1,5 +1,10 @@
 package loianeGroner_EstruturaDeDadosComJAVA;
 
 public class Vetor {
+	private String[] elementos;
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
 
 }
