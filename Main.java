9a4abcb65f7115ca604e9cddd469b1a071ad8f2b
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2022-03-19 23:11:59 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,21 @@ package loianeGroner_EstruturaDeDadosComJAVA;
 public class Main {
 
 	public static void main(String[] args) {
-		Vetor vetor = new Vetor(5);
+		double[] temperatura = new double[10];
 
-		int i = 0;
-		i++;
+		temperatura[0] = 18.6;
+		temperatura[1] = 22.0;
+		temperatura[2] = 23.6;
+		temperatura[3] = 27.6;
+		temperatura[4] = 32.6;
+
+		System.out.println("O valor da temperatura hoje é:" + temperatura[4]);
+
+		System.out.println("o tamanho do array é:" + temperatura.length);
+
+		for (int i = 0; i < temperatura.length; i++) {
+			System.out.println("temperatura do dia:" + (i+1) + "E:" + temperatura[1]);
+		}
 
 	}
-
 }
